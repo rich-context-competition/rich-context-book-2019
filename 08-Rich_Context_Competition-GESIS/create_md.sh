9e5a931cb5d449157801743482837c828524d465
Wilhelm Tell <wolfgang.otto@gesis.org>
2019-07-17 13:25:31 +0200

    renamed target markdown

--- 08-Rich_Context_Competition-GESIS/create_md.sh
+++ 08-Rich_Context_Competition-GESIS/create_md.sh
@@ -1,1 +1,1 @@
-pandoc -f latex -t markdown_github -o gesis.md main.tex --bibliography bibliography.bib --csl sage-harvard.csl
+pandoc -f latex -t markdown_github -o 08-Rich_Context_Competition-GESIS.md main.tex --bibliography bibliography.bib --csl sage-harvard.csl
